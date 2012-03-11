5850cebd91b563436772a6ce484efbf2cc539f89
baanano <baanano@f0f967ba-f53c-4532-aa16-36046de66d7e>
2012-03-11 18:53:14 +0000

    commit

--- trunk/Locale/Russian.lua
+++ trunk/Locale/Russian.lua
@@ -1,44 +1,7 @@
 ﻿local _, InternalInterface = ...
 
-InternalInterface.Localization.RegisterLocale("Russian",
+InternalInterface.Localization.RegisterLocale("Russian",--@do-not-package@
 {
-	-- Posting panel
-	["labelStackSize"] = "Размер стака",
-	["labelStackNumber"] = "Количество стаков",
-	["labelUnitBid"] = "Начальная ставка:",
-	["labelUnitBuy"] = "Выкуп:",
-	["labelDuration"] = "Продолжительность:",
-	["labelDurationFormat"] = "%d часов",
-	["buttonUndercut"] = "Низшая цена",
-	["buttonPost"] = "Разместить",
-
-	-- Auctions panel
-	["columnSeller"] = "Продавец",
-	["columnStack"] = "Кол-во",
-	["columnBid"] = "Ставка",
-	["columnBuy"] = "Выкуп",
-	["columnBidPerUnit"] = "Ставка за 1",
-	["columnBuyPerUnit"] = "Выкуп за 1",
-	["columnMinExpire"] = "Мин.время",
-	["columnMaxExpire"] = "Макс.время",
-	["lastUpdateMessage"] = "Последнее сканирование: ",
-	["lastUpdateDateFormat"] = "%A %x, %X",
-	["lastUpdateDateFallback"] = "Никогда",
-	["buttonBid"] = "Ставка",
-	["buttonBuy"] = "Выкупить",
-	
-	-- Messages
-	["itemScanStarted"] = "Сканирование начато",
-	["itemScanError"] = "Не могу запустить сканирование этого товара, попробуйте позже.",
-	["fullScanStarted"] = "Сканирование начато",
-	["fullScanError"] = "Не могу запустить полное сканирование, попробуйте позже.",
-	["slashRegisterError"] = "Не могу выполнить /<команду>",
-	
-	["scanMessage"] = "%s сканирование: всего %d аукционов%s%s%s.",
-	["scanTypeFull"] = "Полное",
-	["scanTypePartial"] = "Частичное",
-	["scanNewCount"] = ", %d новых",
-	["scanUpdatedCount"] = ", %d обновлено",
-	["scanRemovedCount"] = ", %d удалено (%d до истечения)",
-})
-
+}--@end-do-not-package@
+--@localization(locale="ruRU", format="lua_table")@
+)
