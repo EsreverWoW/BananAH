b416704cfeebe0890baac3f2eeb02fbb7773291d
baanano <baanano@f0f967ba-f53c-4532-aa16-36046de66d7e>
2012-03-11 18:37:05 +0000

    commit

--- trunk/Locale/German.lua
+++ trunk/Locale/German.lua
@@ -1,44 +1,9 @@
 ﻿local _, InternalInterface = ...
 
 InternalInterface.Localization.RegisterLocale("German",
+--@do-not-package@
 {
-	-- Posting panel
-	["labelStackSize"] = "Stapelgröße",
-	["labelStackNumber"] = "Anzahl Stapel",
-	["labelUnitBid"] = "EP Bieten:",
-	["labelUnitBuy"] = "EP Sofortkauf:",
-	["labelDuration"] = "Restzeit:",
-	["labelDurationFormat"] = "%d Stunden",
-	["buttonUndercut"] = "unterbieten",
-	["buttonPost"] = "Anbieten",
-
-	-- Auctions panel
-	["columnSeller"] = "Verkäufer",
-	["columnStack"] = "Einheiten",
-	["columnBid"] = "Bieten",
-	["columnBuy"] = "Sofortkauf",
-	["columnBidPerUnit"] = "EP Bieten",
-	["columnBuyPerUnit"] = "EP Sofortkauf",
-	["columnMinExpire"] = "Min. Auslaufend",
-	["columnMaxExpire"] = "Max. Auslaufend",
-	["lastUpdateMessage"] = "Letztes Update: ",
-	["lastUpdateDateFormat"] = "%A %x, %X",
-	["lastUpdateDateFallback"] = "Niemals",
-	["buttonBid"] = "Bieten",
-	["buttonBuy"] = "Sofortkaufen",
-   
-	-- Messages
-	["itemScanStarted"] = "Itemscan gestartet",
-	["itemScanError"] = "Fehler.",
-	["fullScanStarted"] = "Vollscan gestartet",
-	["fullScanError"] = "Fehler.",
-	["slashRegisterError"] = "Kommandos konnten nicht registriert werden.",
-   
-	["scanMessage"] = "%s Scan: %d Gesamt%s%s%s.",
-	["scanTypeFull"] = "Voll",
-	["scanTypePartial"] = "Item",
-	["scanNewCount"] = ", %d Neu",
-	["scanUpdatedCount"] = ", %d aktualisiert",
-	["scanRemovedCount"] = ", %d entfernt (%d kurz vor dem auslaufen)",
-})
-
+}
+--@end-do-not-package@
+--@localization(locale="deDE", format="lua_table")
+)
