6fb96a50223cd29f099cf622246605b0ef98e62a
baanano <baanano@f0f967ba-f53c-4532-aa16-36046de66d7e>
2012-03-11 19:05:23 +0000

    commit

--- trunk/Locale/German.lua
+++ trunk/Locale/German.lua
@@ -3,5 +3,5 @@
 InternalInterface.Localization.RegisterLocale("German",--@do-not-package@
 {
 }--@end-do-not-package@
---@localization(locale="deDE", format="lua_table")@
+--@localization(locale="deDE", format="lua_table", handle-subnamespaces="concat", namespace-delimiter=".")@
 )
--- trunk/Locale/Russian.lua
+++ trunk/Locale/Russian.lua
@@ -3,5 +3,5 @@
 InternalInterface.Localization.RegisterLocale("Russian",--@do-not-package@
 {
 }--@end-do-not-package@
---@localization(locale="ruRU", format="lua_table")@
+--@localization(locale="ruRU", format="lua_table", handle-subnamespaces="concat", namespace-delimiter=".")@
 )
