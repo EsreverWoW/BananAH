8b53353b74ce288941a658a99760ce5ea2dc2765
baanano <baanano@f0f967ba-f53c-4532-aa16-36046de66d7e>
2012-03-11 18:48:47 +0000

    commit

--- Locale/German.lua
+++ Locale/German.lua
@@ -2,5 +2,6 @@
 
 InternalInterface.Localization.RegisterLocale("German",--@do-not-package@
 {
-}--@end-do-not-package@--@localization(locale="deDE", format="lua_table")@
+}--@end-do-not-package@
+--@localization(locale="deDE", format="lua_table")@
 )
