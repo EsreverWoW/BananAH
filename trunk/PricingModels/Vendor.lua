40ce7ed75642a6314cd8712db7331d123c9863e4
baanano <baanano@f0f967ba-f53c-4532-aa16-36046de66d7e>
2012-04-26 22:41:04 +0000

    commit

--- trunk/PricingModels/Vendor.lua
+++ trunk/PricingModels/Vendor.lua
@@ -9,7 +9,7 @@ local PRICING_MODEL_NAME = L["PricingModel/fallbackName"]
 local function PricingModel(item, auctions, autoMode)
 	local ok, itemDetail = pcall(Inspect.Item.Detail, item)
 	
-	local sellPrice = ok and itemDetail.sell or 1
+	local sellPrice = ok and itemDetail and itemDetail.sell or 1
 	local bid = math.floor(sellPrice * 3)
 	local buyout = math.floor(sellPrice * 5)
 	
