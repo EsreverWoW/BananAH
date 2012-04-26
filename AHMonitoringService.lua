8c13ab940034f122c5680adb73bb5c47d18939c6
baanano <baanano@f0f967ba-f53c-4532-aa16-36046de66d7e>
2012-04-26 22:41:04 +0000

    commit

--- AHMonitoringService.lua
+++ AHMonitoringService.lua
@@ -1,7 +1,7 @@
 local addonInfo, InternalInterface = ...
 local addonID = addonInfo.identifier
 
-local MAX_DATA_AGE = 1 * 24 * 60 * 60
+local MAX_DATA_AGE = 30 * 24 * 60 * 60
 
 local auctionTable = {}
 local auctionTableLoaded = false
@@ -11,6 +11,159 @@ local auctionSearcher = InternalInterface.Utility.BuildAuctionTree()
 local AuctionDataEvent = Utility.Event.Create(addonID, "AuctionData")
 local backgroundScannerDisabled = false
 local scanNext = false
+
+local function UnpackAuctionTable(packedDB)
+	if type(packedDB) ~= "table" or not packedDB.DICT then return packedDB end
+	
+	local unpackedDB = {}
+
+	local dictionary = {}
+	local sDictionary = (packedDB.DICT or "") .. "#"
+	sDictionary:gsub("([^#]*)#", function(c) dictionary[#dictionary + 1] = c end)
+	setmetatable(dictionary, { __index = function(tab, key) return rawget(tab, tonumber(key, 16)) or key end, })
+	
+	for _, itemPackedData in ipairs(packedDB) do
+		local itemData = {}
+		local sItemData = itemPackedData .. "#"
+		sItemData:gsub("([^#]*)#", function(c) itemData[#itemData + 1] = c end)
+		
+		local itemType = itemData[1] .. ","
+		itemType = { itemType:match((itemType:gsub("[^,]*,", "([^,]*),"))) }
+		itemType = table.concat({ dictionary[itemType[1]], itemType[2], dictionary[itemType[3]], dictionary[itemType[4]], dictionary[itemType[5]], dictionary[itemType[6]], dictionary[itemType[7]], itemType[8], }, ",")
+
+		local name = dictionary[itemData[2]]
+		local rarity = dictionary[itemData[3]]
+		local level = tonumber(dictionary[itemData[4]], 16)
+		local category = dictionary[itemData[5]]
+		local callings = tonumber(dictionary[itemData[6]], 16)
+		callings = { warrior = bit.band(callings, 8) > 0 and true or nil, cleric = bit.band(callings, 4) > 0 and true or nil, rogue = bit.band(callings, 2) > 0 and true or nil, mage = bit.band(callings, 1) > 0 and true or nil, }
+		
+		local auctions = {}
+		for index = 7, #itemData do
+			local auctionData = itemData[index] .. ","
+			auctionData = { auctionData:match((auctionData:gsub("[^,]+,", "([^,]*),"))) }
+			
+			local auctionID = string.format("o%08s%08s", dictionary[auctionData[1]], auctionData[2])
+			local buy = tonumber(dictionary[auctionData[5]], 16)
+			local rbe = tonumber(auctionData[12])
+			auctions[auctionID] = 
+			{ 
+				buy = buy, rbe = rbe, sln = dictionary[auctionData[6]], bdd = tonumber(auctionData[11]),
+				stk = tonumber(auctionData[3], 16), bid = tonumber(dictionary[auctionData[4]], 16), 	
+				fst = tonumber(dictionary[auctionData[7]], 16), lst = tonumber(dictionary[auctionData[8]], 16),
+				met = tonumber(dictionary[auctionData[9]], 16), xet = tonumber(dictionary[auctionData[10]], 16),
+			}
+		end
+
+		unpackedDB[itemType] =
+		{
+			name = name,
+			rarity = rarity,
+			level = level,
+			category = category,
+			callings = callings,
+			auctions = auctions,
+		}
+	end
+	
+	return unpackedDB
+end
+
+
+local function PackAuctionTable()
+	local packedDB = {}
+	
+	local encodeAlways = {}
+	local function EncodeAlways(key)
+		if key ~= "" then
+			if type(key) == "number" then
+				key = string.format("%X", key)
+			end
+			encodeAlways[key] = (encodeAlways[key] or 0) + 1 
+		end 
+		return key 
+	end
+	
+	local encodeWhenRepeated = {}
+	local function EncodeWhenRepeated(key)
+		encodeWhenRepeated[key] = (encodeWhenRepeated[key] or 0) + 1
+		return key
+	end		
+	
+	for itemType, itemData in pairs(auctionTable) do
+		local packedAuctions = {}
+		
+		for auctionID, auctionData in pairs(itemData.auctions) do
+			local packedAuctionData = 
+			{
+				EncodeAlways(tonumber(auctionID:sub(2, 9), 16)),
+				string.format("%X", tonumber(auctionID:sub(10), 16)),
+				string.format("%X", auctionData.stk),
+				EncodeAlways(auctionData.bid),
+				EncodeAlways(auctionData.buy),
+				EncodeWhenRepeated(auctionData.sln),
+				EncodeAlways(auctionData.fst),
+				EncodeAlways(auctionData.lst),
+				EncodeAlways(auctionData.met),
+				EncodeAlways(auctionData.xet),
+				auctionData.bdd,
+				auctionData.rbe,
+			}
+			table.insert(packedAuctions, packedAuctionData)					
+		end
+
+		if #packedAuctions > 0 then
+			local packedItemType = itemType .. ","
+			packedItemType = { packedItemType:match((packedItemType:gsub("[^,]*,", "([^,]*),"))) }
+			packedItemType =
+			{
+				EncodeWhenRepeated(packedItemType[1]), 
+				packedItemType[2], 
+				EncodeAlways(packedItemType[3]), 
+				EncodeAlways(packedItemType[4]), 
+				EncodeAlways(packedItemType[5]), 
+				EncodeAlways(packedItemType[6]), 
+				EncodeWhenRepeated(packedItemType[7]), 
+				packedItemType[8],
+			}
+			local packedItemData =
+			{
+				packedItemType,
+				EncodeWhenRepeated(itemData.name),
+				EncodeWhenRepeated(itemData.rarity),
+				EncodeAlways(itemData.level),
+				EncodeWhenRepeated(itemData.category),
+				EncodeAlways((itemData.callings.warrior and 8 or 0) + (itemData.callings.cleric and 4 or 0) + (itemData.callings.rogue and 2 or 0) + (itemData.callings.mage and 1 or 0)),
+				packedAuctions,
+			}
+			table.insert(packedDB, packedItemData)
+		end
+	end
+	
+	for key, frequency in pairs(encodeWhenRepeated) do if frequency > 1 then EncodeAlways(key) end end
+	encodeWhenRepeated = {}
+	for key, frequency in pairs(encodeAlways) do table.insert(encodeWhenRepeated, key) end
+	table.sort(encodeWhenRepeated, function(a, b) return encodeAlways[b] < encodeAlways[a] end)
+	for index, key in ipairs(encodeWhenRepeated) do encodeAlways[key] = string.format("%X", index) end
+	setmetatable(encodeAlways, { __index = function(tab, key) return rawget(tab, key) or key end })
+	
+	for index, packedItemData in ipairs(packedDB) do
+		packedDB[index][1] = table.concat({ encodeAlways[packedItemData[1][1]], packedItemData[1][2], encodeAlways[packedItemData[1][3]], encodeAlways[packedItemData[1][4]], encodeAlways[packedItemData[1][5]], encodeAlways[packedItemData[1][6]], encodeAlways[packedItemData[1][7]], packedItemData[1][8], }, ",")
+		packedDB[index][2] = encodeAlways[packedItemData[2]]
+		packedDB[index][3] = encodeAlways[packedItemData[3]]
+		packedDB[index][4] = encodeAlways[packedItemData[4]]
+		packedDB[index][5] = encodeAlways[packedItemData[5]]
+		packedDB[index][6] = encodeAlways[packedItemData[6]]
+		for aIndex, packedAuctionData in ipairs(packedItemData[7]) do
+			packedDB[index][7][aIndex] = table.concat({ encodeAlways[packedAuctionData[1]], packedAuctionData[2], packedAuctionData[3], encodeAlways[packedAuctionData[4]], encodeAlways[packedAuctionData[5]], encodeAlways[packedAuctionData[6]], encodeAlways[packedAuctionData[7]], encodeAlways[packedAuctionData[8]], encodeAlways[packedAuctionData[9]], encodeAlways[packedAuctionData[10]], encodeAlways[packedAuctionData[11]], encodeAlways[packedAuctionData[12]], }, ",")
+		end
+		packedDB[index][7] = table.concat(packedDB[index][7], "#")
+		packedDB[index] = table.concat(packedDB[index], "#")
+	end
+	packedDB.DICT = table.concat(encodeWhenRepeated, "#")
+		
+	return packedDB
+end
 
 local function UpsertAuction(auctionID, auctionDetail, auctionScanTime, expireTimes)
 	local itemType = auctionDetail.itemType
@@ -157,7 +310,9 @@ table.insert(Event.Auction.Scan, { OnAuctionData, addonID, "AHMonitoringService.
 local function LoadAuctionTable(addonId)
 	if addonId == addonID then
 		if type(_G[addonID .. "AuctionTable"]) == "string" then
-			auctionTable = loadstring("return " .. zlib.inflate()(_G[addonID .. "AuctionTable"]))()
+			auctionTable = loadstring("return " .. zlib.inflate()(_G[addonID .. "AuctionTable"]))
+			auctionTable = auctionTable and auctionTable() or {}
+			auctionTable = UnpackAuctionTable(auctionTable)
 		else
 			auctionTable = {}
 		end
@@ -191,7 +346,7 @@ local function SaveAuctionTable(addonId)
 			end
 		end
 		
-		_G[addonID .. "AuctionTable"] = zlib.deflate(zlib.BEST_COMPRESSION)(Utility.Serialize.Inline(auctionTable), "finish")
+		_G[addonID .. "AuctionTable"] = zlib.deflate(zlib.BEST_COMPRESSION)(Utility.Serialize.Inline(PackAuctionTable()), "finish")
 	end
 end
 table.insert(Event.Addon.SavedVariables.Save.Begin, {SaveAuctionTable, addonID, "AHMonitoringService.SaveAuctionData"})
