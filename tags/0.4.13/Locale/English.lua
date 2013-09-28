8958eecf6cbfef2bbaa80487f232454dd9679f2a
baanano <baanano@f0f967ba-f53c-4532-aa16-36046de66d7e>
2013-09-28 16:31:58 +0000

    Tagging as 0.4.13

---
+++ tags/0.4.13/Locale/English.lua
@@ -0,0 +1,277 @@
+﻿local _, InternalInterface = ...
+
+InternalInterface.Localization.RegisterLocale("English",--@do-not-package@
+{
+	-- General
+	["General/CompetitionName1"] = "Weak",
+	["General/CompetitionName2"] = "Moderate",
+	["General/CompetitionName3"] = "Intense",
+	["General/CompetitionName4"] = "Strong",
+	["General/CompetitionName5"] = "Fierce",
+	
+	["General/ScoreName0"] = "No score",
+	["General/ScoreName1"] = "Very low",
+	["General/ScoreName2"] = "Low",
+	["General/ScoreName3"] = "Medium",
+	["General/ScoreName4"] = "High",
+	["General/ScoreName5"] = "Very high",
+	
+	["General/CallingAll"] = "All",
+	["General/CallingWarrior"] = "Warrior",
+	["General/CallingCleric"] = "Cleric",
+	["General/CallingRogue"] = "Rogue",
+	["General/CallingMage"] = "Mage",
+	
+	["General/Rarity1"] = "Sellable",
+	["General/Rarity2"] = "Common",
+	["General/Rarity3"] = "Uncommon",
+	["General/Rarity4"] = "Rare",
+	["General/Rarity5"] = "Epic",
+	["General/Rarity6"] = "Relic",
+	["General/Rarity7"] = "Transcendant",
+	["General/RarityQuest"] = "Quest",
+	
+	["General/ModelTypeSimple"] = "Simple",
+	["General/ModelTypeStatistical"] = "Statistical",
+	["General/ModelTypeComplex"] = "Complex",
+	["General/ModelTypeComposite"] = "Composite",
+
+	-- Main window
+	["Main/MenuSearch"] = "SEARCH",
+	["Main/MenuPost"] = "POST",
+	["Main/MenuAuctions"] = "SELLING",
+	["Main/MenuBids"] = "TRACKING",
+	["Main/MenuMap"] = "MAP", -- NEW
+	["Main/MenuHistory"] = "HISTORY",
+	["Main/MenuConfig"] = "CONFIG",
+	["Main/MenuFullScan"] = "FULL SCAN",
+	
+	["Main/ScanMessage"] = "Scan results: %d total auctions%s%s%s.",
+	["Main/ScanNewCount"] = ", %d new",
+	["Main/ScanUpdatedCount"] = ", %d updated",
+	["Main/ScanRemovedCount"] = ", %d removed (%d before expiration)",
+	
+	["Main/FullScanStarted"] = "Full scan started...",
+	["Main/FullScanError"] = "Can't issue a full scan right now, try again later.",
+	["Main/SlashRegisterError"] = "Failed to register slash commands.",
+	
+	-- Save Search Popup
+	["SaveSearchPopup/Title"] = "SAVE SEARCH",
+	["SaveSearchPopup/NameText"] = "Saved search name:",
+	["SaveSearchPopup/DefaultName"] = "Saved %x %X",
+	["SaveSearchPopup/ButtonSave"] = "Save",
+	["SaveSearchPopup/ButtonCancel"] = "Cancel",
+	
+	-- Cancel Auction Popup
+	["CancelAuctionPopup/Title"] = "CANCEL AUCTION",
+	["CancelAuctionPopup/ContentText"] = "Are you sure you want to cancel this auction?",
+	["CancelAuctionPopup/ButtonYes"] = "Yes",
+	["CancelAuctionPopup/ButtonNo"] = "No",
+	["CancelAuctionPopup/IgnoreText"] = "Don't ask again",
+	
+	-- Unsaved Changes Popup
+	["UnsavedChangesPopup/Title"] = "WARNING",
+	["UnsavedChangesPopup/ContentText"] = "You have unsaved changes. If you continue, they'll be lost.",
+	["UnsavedChangesPopup/ButtonContinue"] = "Continue",
+	["UnsavedChangesPopup/ButtonCancel"] = "Cancel",
+
+	-- New Model Popup
+	["NewModelPopup/Title"] = "NEW MODEL",
+	["NewModelPopup/ModelType"] = "Model Type:",
+	["NewModelPopup/ButtonContinue"] = "Continue",
+	["NewModelPopup/ButtonCancel"] = "Cancel",
+	
+	-- Matcher Popup Frame
+	["MatcherPopupFrame/Title"] = "Price Matchers",
+	["MatcherPopupFrame/ButtonAdd"] = "Add",
+	
+	-- Filter Popup Frame
+	["FilterPopupFrame/Title"] = "Filters",
+	["FilterPopupFrame/ButtonAdd"] = "Add",
+	
+	-- Simple Model Popup
+	["SimpleModelPopup/Name"] = "Name:",
+	["SimpleModelPopup/ButtonSave"] = "Save",
+	["SimpleModelPopup/ButtonCancel"] = "Cancel",
+	
+	-- Statistical Model Popup
+	["StatisticalModelPopup/Name"] = "Name:",
+	["StatisticalModelPopup/ButtonSave"] = "Save",
+	["StatisticalModelPopup/ButtonCancel"] = "Cancel",
+	
+	-- Complex Model Popup
+	["ComplexModelPopup/Name"] = "Name:",
+	["ComplexModelPopup/ButtonSave"] = "Save",
+	["ComplexModelPopup/ButtonCancel"] = "Cancel",
+	
+	-- Composite Model Popup
+	["CompositeModelPopup/Name"] = "Name:",
+	["CompositeModelPopup/ButtonAdd"] = "Add",
+	["CompositeModelPopup/ButtonSave"] = "Save",
+	["CompositeModelPopup/ButtonCancel"] = "Cancel",
+	
+	-- Item Auctions Grid
+	["ItemAuctionsGrid/ButtonBuy"] = "Buy",
+	["ItemAuctionsGrid/ButtonBid"] = "Bid",
+	["ItemAuctionsGrid/ColumnSeller"] = "Seller",
+	["ItemAuctionsGrid/ColumnStack"] = "Units",
+	["ItemAuctionsGrid/ColumnBid"] = "Bid",
+	["ItemAuctionsGrid/ColumnBuy"] = "Buyout",
+	["ItemAuctionsGrid/ColumnBidPerUnit"] = "Unit Bid",
+	["ItemAuctionsGrid/ColumnBuyPerUnit"] = "Unit Buyout",
+	["ItemAuctionsGrid/ColumnMinExpire"] = "Min. Expire",
+	["ItemAuctionsGrid/ColumnMaxExpire"] = "Max. Expire",
+	["ItemAuctionsGrid/ColumnScore"] = "Score",
+	["ItemAuctionsGrid/ErrorNoAuction"] = "No auction selected",
+	["ItemAuctionsGrid/ErrorNoAuctionHouse"] = "Not at the auction house",
+	["ItemAuctionsGrid/ErrorBidEqualBuy"] = "Bid & Buyout prices are equal",
+	["ItemAuctionsGrid/ErrorSeller"] = "You're the seller",
+	["ItemAuctionsGrid/ErrorHighestBidder"] = "You're the highest bidder",
+	["ItemAuctionsGrid/ErrorNotCached"] = "Need scan refresh",
+	["ItemAuctionsGrid/LastUpdateMessage"] = "Last time seen: %s",
+	["ItemAuctionsGrid/LastUpdateDateFormat"] = "%A %x, %X",
+	["ItemAuctionsGrid/LastUpdateDateFallback"] = "Never",
+	["ItemAuctionsGrid/ItemScanError"] = "Can't issue an item scan right now, try again later.",
+	["ItemAuctionsGrid/ItemScanStarted"] = "Item scan started...",
+	
+	-- Search frame
+	["SearchFrame/ButtonReset"] = "Reset",
+	["SearchFrame/ButtonSearch"] = "Search",
+	["SearchFrame/ButtonTrack"] = "Track auction",
+	["SearchFrame/ButtonBuy"] = "Buy",
+	["SearchFrame/ButtonBid"] = "Bid",
+	["SearchFrame/ColumnItem"] = "Item",
+	["SearchFrame/ColumnSeller"] = "Seller",
+	["SearchFrame/ColumnMinExpire"] = "Min. expire",
+	["SearchFrame/ColumnMaxExpire"] = "Max. expire",
+	["SearchFrame/ColumnBid"] = "Bid",
+	["SearchFrame/ColumnBuy"] = "Buyout",
+	["SearchFrame/ColumnBidPerUnit"] = "Unit Bid",
+	["SearchFrame/ColumnBuyPerUnit"] = "Unit Buyout",
+	["SearchFrame/ColumnScore"] = "Score",
+	["SearchFrame/ErrorNoAuction"] = "No auction selected",
+	["SearchFrame/ErrorNoAuctionHouse"] = "Not at the auction house",
+	["SearchFrame/ErrorBidEqualBuy"] = "Bid & Buyout prices are equal",
+	["SearchFrame/ErrorSeller"] = "You're the seller",
+	["SearchFrame/ErrorHighestBidder"] = "You're the highest bidder",
+	["SearchFrame/ErrorNotCached"] = "Need scan refresh",
+	
+	-- Post frame
+	["PostFrame/LabelItemStack"] = "You have %d available to auction",
+	["PostFrame/LabelStackSize"] = "STACK SIZE:",
+	["PostFrame/LabelAuctionLimit"] = "MAX. AUCTIONS:",
+	["PostFrame/LabelIncompleteStack"] = "Post incomplete stack",
+	["PostFrame/LabelDuration"] = "DURATION:",
+	["PostFrame/ButtonPost"] = "Post",
+	["PostFrame/ButtonReset"] = "Reset",
+	["PostFrame/CheckPriceMatching"] = "Apply matching rules",
+	["PostFrame/CheckBindPrices"] = "Bind prices",
+	["PostFrame/LabelPricingModel"] = "REFERENCE PRICE:",
+	["PostFrame/LabelUnitBid"] = "UNIT BID:",
+	["PostFrame/LabelUnitBuy"] = "UNIT BUYOUT:",
+	["PostFrame/LabelAuctions"] = "%d %s will be posted (%d active, %d queued)",
+	["PostFrame/LabelAuctionsSingular"] = "auction",
+	["PostFrame/LabelAuctionsPlural"] = "auctions",
+	["PostFrame/ErrorPostBase"] = "Couldn't post the item: %s.",
+	["PostFrame/ErrorPostStackSize"] = "Invalid stack size",
+	["PostFrame/ErrorPostStackNumber"] = "No stacks to post",
+	["PostFrame/ErrorPostBidPrice"] = "No bid price",
+	["PostFrame/ErrorPostBuyPrice"] = "Buy price less than bid price",
+	["PostFrame/ErrorAutoPostModelMissing"] = "Auto post disabled: Reference price unavailable",
+	
+	-- Selling frame
+	["SellingFrame/ColumnItem"] = "Item",
+	["SellingFrame/ColumnMinExpire"] = "Min. Expire",
+	["SellingFrame/ColumnMaxExpire"] = "Max. Expire",
+	["SellingFrame/ColumnBid"] = "Bid",
+	["SellingFrame/ColumnBuy"] = "Buyout",
+	["SellingFrame/ColumnBidPerUnit"] = "Unit Bid",
+	["SellingFrame/ColumnBuyPerUnit"] = "Unit Buyout",	
+	["SellingFrame/ColumnScore"] = "Score",
+	["SellingFrame/ColumnCompetition"] = "Competition",
+	["SellingFrame/FilterSeller"] = "Show only auctions posted by this character",
+	["SellingFrame/FilterCompetition"] = "Min. Competition:",
+	["SellingFrame/FilterBelow"] = "Min. Below:",
+	["SellingFrame/FilterScore"] = "SCORE FILTER",
+	
+	-- Config frame
+	["ConfigFrame/CategoryGeneral"] = "GENERAL",
+	["ConfigFrame/CategorySearch"] = "SEARCH",
+	["ConfigFrame/CategoryPost"] = "POST",
+	["ConfigFrame/CategorySelling"] = "SELLING",
+	["ConfigFrame/CategoryTracking"] = "TRACKING",
+	["ConfigFrame/CategoryMap"] = "MAP", -- NEW
+	["ConfigFrame/CategoryHistory"] = "HISTORY",
+	["ConfigFrame/CategoryPricing"] = "PRICES",
+	["ConfigFrame/CategoryScoring"] = "SCORE",
+	
+	["ConfigGeneral/MapIconShow"] = "Show map icon (ignored if using Docker)",
+	["ConfigGeneral/AutoOpenWindow"] = "Open the addon window when the native Auction House window is opened",
+	["ConfigGeneral/AutoCloseWindow"] = "Close the addon window when the native Auction House window is closed",
+	["ConfigGeneral/PausePostingQueue"] = "Pause posting queue on load (note other Auction House addons may override this setting)",
+	
+	["ConfigSearch/DefaultSearcher"] = "Default searcher:",
+	["ConfigSearch/DefaultSearchMode"] = "Default search mode:",
+	
+	["ConfigPost/RarityFilter"] = "Minimum rarity filter:",
+	["ConfigPost/DefaultReferencePrice"] = "Default reference price:",
+	["ConfigPost/FallbackReferencePrice"] = "Fallback reference price:",
+	["ConfigPost/BidPercentage"] = "Bid percentage (of buyout price):",
+	["ConfigPost/DefaultBindPrices"] = "Bind bid & buyout prices for unconfigured items",
+	["ConfigPost/UndercutAbsolute"] = "Amount to undercut on right click:",
+	["ConfigPost/UndercutPercentage"] = "Percentage to undercut on right click:",
+	["ConfigPost/AutoPostPause"] = "Pause the posting queue when autoposting",
+	
+	["ConfigSelling/BypassCancelPopup"] = "Cancel auctions with a single click (don't ask for confirmation)",
+	["ConfigSelling/FilterRestrictCharacter"] = "Show only auctions posted by the current character by default",
+	["ConfigSelling/FilterCompetition"] = "Default min. competition:",
+	["ConfigSelling/FilterBelow"] = "Default min. auctions below:",
+	["ConfigSelling/FilterScore"] = "Default scores:",
+	
+	["ConfigPrice/ItemCategory"] = "Item category",
+	["ConfigPrice/CategoryConfig"] = "Category config.",
+	["ConfigPrice/CategoryInherit"] = "Inherit from parent category",
+	["ConfigPrice/CategoryOwn"] = "Define custom parameters for this category",
+	["ConfigPrice/DefaultStackSize"] = "Default stack size",
+	["ConfigPrice/DefaultMaxAuctions"] = "Default max. auctions",
+	["ConfigPrice/DefaultDuration"] = "Default duration",
+	["ConfigPrice/ButtonSave"] = "Save",
+	["ConfigPrice/ColumnReferencePrice"] = "Reference Price",
+	["ConfigPrice/ColumnActive"] = "Active",
+	["ConfigPrice/ColumnDefault"] = "Default",
+	["ConfigPrice/ColumnFallback"] = "Fallback",
+	["ConfigPrice/ButtonDelete"] = "Delete",
+	["ConfigPrice/ButtonEdit"] = "Edit",
+	["ConfigPrice/ButtonNew"] = "New",
+	["ConfigPrice/ApplyMatching"] = "Apply matching rules for unconfigured items",
+	
+	["ConfigScore/ReferencePrice"] = "Reference price for scoring:",
+	
+	-- Misc
+	["Misc/DateMonthAbbreviatedNames"] = "Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec",
+	["Misc/DateMonthNames"] = "January,February,March,April,May,June,July,August,September,October,November,December",
+	["Misc/DateWeekdayAbbreviatedNames"] = "Sun,Mon,Tue,Wed,Thu,Fri,Sat",
+	["Misc/DateWeekdayNames"] = "Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday",
+	["Misc/RemainingTimeHours"] = "%d h %d m",
+	["Misc/RemainingTimeMinutes"] = "%d m %d s",
+	["Misc/RemainingTimeSeconds"] = "%d s",
+	["Misc/SearchModeOnline"] = "Online",
+	["Misc/SearchModeOffline"] = "Offline",
+	["Misc/StackSizeMax"] = "Max",
+	["Misc/AuctionLimitMax"] = "All",
+	["Misc/StackSizeMaxKeyShortcut"] = "+",
+	["Misc/AuctionLimitMaxKeyShortcut"] = "+",
+	["Misc/DurationFormat"] = "%d hours",
+	
+	-- Default price models
+	["PriceModels/Fixed"] = "User defined",
+	["PriceModels/Vendor"] = "Vendor",
+	["PriceModels/Average"] = "Average",
+	["PriceModels/Median"] = "Median",
+	["PriceModels/StandardDeviation"] = "Standard deviation",
+	["PriceModels/TrimmedMean"] = "Trimmed average",
+	["PriceModels/Market"] = "Market price",
+}--@end-do-not-package@
+--@localization(locale="enUS", format="lua_table", handle-subnamespaces="concat", namespace-delimiter="/")@
+)
+
---
+++ tags/0.4.13/Main.lua
@@ -0,0 +1,244 @@
+-- ***************************************************************************************************************************************************
+-- * Main.lua                                                                                                                                        *
+-- ***************************************************************************************************************************************************
+-- * Creates the addon windows                                                                                                                       *
+-- ***************************************************************************************************************************************************
+-- * 0.4.1 / 2012.07.29 / Baanano: Updated for 0.4.1                                                                                                 *
+-- ***************************************************************************************************************************************************
+
+local addonInfo, InternalInterface = ...
+local addonID = addonInfo.identifier
+local PublicInterface = _G[addonID]
+
+local CAScan = Command.Auction.Scan
+local CSRegister = Command.Slash.Register
+local L = InternalInterface.Localization.L
+local Panel = Yague.Panel
+local PopupManager = Yague.PopupManager
+local SFormat = string.format
+local TInsert = table.insert
+local UICreateContext = UI.CreateContext
+local UICreateFrame = UI.CreateFrame
+local UNMapMini = UI.Native.MapMini
+local UNAuction = UI.Native.Auction
+local Write = InternalInterface.Output.Write
+local pcall = pcall
+local tostring = tostring
+
+local MIN_WIDTH = 1370
+local MIN_HEIGHT = 800
+local DEFAULT_WIDTH = 1370
+local DEFAULT_HEIGHT = 800
+
+local function InitializeLayout()
+	local mapContext = UICreateContext(addonID .. ".UI.MapContext")
+	local mapIcon = UICreateFrame("Texture", addonID .. ".UI.MapIcon", mapContext)
+
+	local mainContext = UICreateContext(addonID .. ".UI.MainContext")
+
+	local mainWindow = Yague.Window(addonID .. ".UI.MainWindow", mainContext)
+	local popupManager = PopupManager(mainWindow:GetName() .. ".PopupManager", mainWindow)
+	local mainTab = Yague.TabControl(mainWindow:GetName() .. ".MainTab", mainWindow:GetContent())
+	local searchFrame = InternalInterface.UI.SearchFrame(mainTab:GetName() .. ".SearchFrame", mainTab:GetContent())
+	local postFrame = InternalInterface.UI.PostFrame(mainTab:GetName() .. ".PostFrame", mainTab:GetContent())
+	local sellingFrame = InternalInterface.UI.SellingFrame(mainTab:GetName() .. ".SellingFrame", mainTab:GetContent())
+	local mapFrame = InternalInterface.UI.MapFrame(mainTab:GetName() .. ".MapFrame", mainTab:GetContent())
+	local configFrame = InternalInterface.UI.ConfigFrame(mainTab:GetName() .. ".ConfigFrame", mainTab:GetContent())
+
+	local queueManager = InternalInterface.UI.QueueManager(mainWindow:GetName() .. ".QueueManager", mainWindow:GetContent())
+	
+	local statusPanel = Panel(addonID .. ".UI.MainWindow.StatusBar", mainWindow:GetContent())
+	local statusText = UICreateFrame("Text", addonID .. ".UI.MainWindow.StatusText", statusPanel:GetContent())
+
+	local refreshPanel = Panel(mainWindow:GetName() .. ".RefreshPanel", mainWindow:GetContent())
+	local refreshText = Yague.ShadowedText(mainWindow:GetName() .. ".RefreshText", refreshPanel:GetContent())
+	
+	local refreshEnabled = false
+
+	local function ShowSelectedFrame(frame)
+		if frame and frame.Show then
+			frame:Show()
+		end
+	end
+	
+	local function HideSelectedFrame(frame)
+		if frame and frame.Hide then
+			frame:Hide()
+		end
+	end
+	
+	local function ShowBananAH()
+		mainContext:SetLayer(UI.Native.Auction:GetLayer() + 1)
+		mainWindow:SetVisible(true)
+		if mainWindow:GetTop() < 0 then
+			mainWindow:ClearAll()
+			mainWindow:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+			mainWindow:SetWidth(DEFAULT_WIDTH)
+			mainWindow:SetHeight(DEFAULT_HEIGHT)
+		end
+		ShowSelectedFrame(mainTab:GetSelectedFrame())
+	end	
+	
+	mapContext:SetStrata("hud")
+
+	mapIcon:SetTextureAsync(addonID, "Textures/MapIcon.png")
+	InternalInterface.UI.MapIcon = mapIcon
+	if MINIMAPDOCKER then
+		MINIMAPDOCKER.Register(addonID, mapIcon)
+	else
+		mapIcon:SetVisible(InternalInterface.AccountSettings.General.ShowMapIcon or false)
+		mapIcon:SetPoint("CENTER", UNMapMini, "BOTTOMLEFT", 24, -25)
+	end
+	
+	mainWindow:SetVisible(false)
+	mainWindow:SetMinWidth(MIN_WIDTH)
+	mainWindow:SetMinHeight(MIN_HEIGHT)
+	mainWindow:SetWidth(DEFAULT_WIDTH)
+	mainWindow:SetHeight(DEFAULT_HEIGHT)
+	mainWindow:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+	mainWindow:SetTitle(addonID)
+	mainWindow:SetAlpha(1)
+	mainWindow:SetCloseable(true)
+	mainWindow:SetDraggable(true)
+	mainWindow:SetResizable(false)
+	
+	popupManager:SetAllPoints(mainWindow:GetContent())
+	
+	mainTab:SetPoint("TOPLEFT", mainWindow:GetContent(), "TOPLEFT", 5, 5)
+	mainTab:SetPoint("BOTTOMRIGHT", mainWindow:GetContent(), "BOTTOMRIGHT", -5, -40)
+	mainTab:AddTab("search", L["Main/MenuSearch"], searchFrame)
+	mainTab:AddTab("post", L["Main/MenuPost"], postFrame)
+	mainTab:AddTab("auctions", L["Main/MenuAuctions"], sellingFrame)
+	--mainTab:AddTab("bids", L["Main/MenuBids"], nil)
+	--mainTab:AddTab("map", L["Main/MenuMap"], mapFrame)
+	--mainTab:AddTab("history", L["Main/MenuHistory"], nil)
+	mainTab:AddTab("config", L["Main/MenuConfig"], configFrame)
+	
+	queueManager:SetPoint("BOTTOMRIGHT", mainWindow:GetContent(), "BOTTOMRIGHT", -5, -5)
+	queueManager:SetPoint("TOPLEFT", mainWindow:GetContent(), "BOTTOMRIGHT", -155, -35)
+
+	statusPanel:SetPoint("TOPLEFT", mainWindow:GetContent(), "BOTTOMLEFT", 5, -35)
+	statusPanel:SetPoint("BOTTOMRIGHT", queueManager, "BOTTOMLEFT", -5, 0)
+	statusPanel:GetContent():SetBackgroundColor(0.2, 0.2, 0.2, 0.5)
+	
+	statusText:SetPoint("CENTERLEFT", statusPanel:GetContent(), "CENTERLEFT", 5, 0)
+	statusText:SetPoint("CENTERRIGHT", statusPanel:GetContent(), "CENTERRIGHT", -5, 0)
+	
+	refreshPanel:SetPoint("TOPRIGHT", mainTab, "TOPRIGHT", -20, 0)
+	refreshPanel:SetBottomBorderVisible(false)
+	refreshPanel:SetHeight(44)
+
+	refreshText:SetPoint("CENTER", refreshPanel:GetContent(), "CENTER")
+	refreshText:SetFontSize(16)
+	refreshText:SetFontColor(0.5, 0.5, 0.5)
+	refreshText:SetShadowOffset(2, 2)
+	refreshText:SetText(L["Main/MenuFullScan"])
+	
+	refreshPanel:SetWidth(refreshText:GetWidth() + 60)
+
+	function UNMapMini.Event:Layer()
+		mapContext:SetLayer(UNMapMini:GetLayer() + 1)
+	end
+
+	function mapIcon.Event:LeftClick()
+		if not mainWindow:GetVisible() then
+			ShowBananAH()
+		else
+			mainWindow:Close()
+		end
+	end
+	
+	function UNAuction.Event:Loaded()
+		if UNAuction:GetLoaded() and InternalInterface.AccountSettings.General.AutoOpen then
+			ShowBananAH()
+		end
+		if not UNAuction:GetLoaded() and InternalInterface.AccountSettings.General.AutoClose then
+			mainWindow:Close()
+		end
+	end
+
+	function mainWindow.Event:Close()
+		HideSelectedFrame(mainTab:GetSelectedFrame())
+		mainWindow:SetKeyFocus(true)
+		mainWindow:SetKeyFocus(false)
+	end
+	
+	function mainTab.Event:TabSelected(id, frame, oldID, oldFrame)
+		ShowSelectedFrame(frame)
+		HideSelectedFrame(oldFrame)
+	end
+	
+	function refreshPanel.Event:MouseIn()
+		refreshText:SetFontSize(refreshEnabled and 18 or 16)
+	end
+	
+	function refreshPanel.Event:MouseOut()
+		refreshText:SetFontSize(16)
+	end
+	
+	function refreshPanel.Event:LeftClick()
+		if not refreshEnabled then return end
+		if not pcall(CAScan, { type = "search", sort = "time", sortOrder = "descending" }) then
+			Write(L["Main/FullScanError"])
+		else
+			Write(L["Main/FullScanStarted"])
+		end	
+	end
+	
+	local function OnInteractionChanged(interaction, state)
+		if interaction == "auction" then
+			refreshEnabled = state
+			refreshText:SetFontColor(0.5, refreshEnabled and 1 or 0.5, 0.5)
+		end
+	end
+	TInsert(Event.Interaction, { OnInteractionChanged, addonID, addonID .. ".OnInteractionChanged" })
+	
+	local function ReportAuctionData(scanType, total, new, updated, removed, before)
+		if scanType ~= "search" then return end
+		local newMessage = (#new > 0) and SFormat(L["Main/ScanNewCount"], #new) or ""
+		local updatedMessage = (#updated > 0) and SFormat(L["Main/ScanUpdatedCount"], #updated) or ""
+		local removedMessage = (#removed > 0) and SFormat(L["Main/ScanRemovedCount"], #removed, #before) or ""
+		local message = SFormat(L["Main/ScanMessage"], #total, newMessage, updatedMessage, removedMessage)
+		Write(message)
+	end
+	TInsert(Event.LibPGC.AuctionData, { ReportAuctionData, addonID, addonID .. ".ReportAuctionData" })
+
+	local slashEvent1 = CSRegister("bananah")
+	local slashEvent2 = CSRegister("bah")
+	
+	if slashEvent1 then
+		TInsert(slashEvent1, {ShowBananAH, addonID, addonID .. ".SlashShow1"})
+	end
+	if slashEvent2 then
+		TInsert(slashEvent2, {ShowBananAH, addonID, addonID .. ".SlashShow2"})
+	elseif not slashEvent1 then
+		print(L["Main/SlashRegisterError"])
+	end
+	
+	local function StatusBarOutput(value)
+		statusText:SetText(value and tostring(value) or "")
+	end
+	InternalInterface.Output.SetOutputFunction(StatusBarOutput)
+	InternalInterface.Output.SetPopupManager(popupManager)
+	
+	local IMHOBAGS = Inspect.Addon.Detail("ImhoBags")
+	if IMHOBAGS and IMHOBAGS.toc and IMHOBAGS.toc.publicAPI == 1 then
+		local function OnImhoBagsRightClick(params)
+			if not params.cancel and mainWindow:GetVisible() then
+				local tabFrame = mainTab:GetSelectedFrame()
+				if tabFrame and tabFrame.ItemRightClick then
+					params.cancel = tabFrame:ItemRightClick(params) and true or false
+				end
+			end			
+		end	
+		TInsert(ImhoBags.Event.Item.Standard.Right, { OnImhoBagsRightClick, addonID, "PostingFrame.OnImhoBagsRightClick" })
+	end
+
+end
+
+local function OnAddonLoaded(addonId)
+	if addonId == addonID then 
+		InitializeLayout()
+	end 
+end
+TInsert(Event.Addon.Load.End, { OnAddonLoaded, addonID, addonID .. ".OnAddonLoaded" })
